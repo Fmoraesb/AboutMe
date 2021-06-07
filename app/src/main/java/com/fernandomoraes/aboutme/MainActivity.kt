b0e872080cdd31f1c32c9e20b1ec2f83eefda95c
Fernando Moraes <fernandombastos@gmail.com>
2021-06-06 22:42:51 -0300

    Adição dos metodos onClick para inserção de um apelido.

--- app/src/main/java/com/fernandomoraes/aboutme/MainActivity.kt
+++ app/src/main/java/com/fernandomoraes/aboutme/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.fernandomoraes.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editText: EditText
+    lateinit var nickNameTextView: TextView
+    lateinit var doneButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btn_feito).setOnClickListener {
+            addNickName(it)
+        }
+        findViewById<TextView>(R.id.tv_nickname).setOnClickListener {
+            updateNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        editText = findViewById(R.id.et_nickname)
+        nickNameTextView = findViewById(R.id.tv_nickname)
+        nickNameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nickNameTextView.visibility = View.VISIBLE
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+    }
+
+    private fun updateNickName(view: View) {
+        editText = findViewById(R.id.et_nickname)
+        doneButton = findViewById(R.id.btn_feito)
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+        editText.requestFocus()
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(editText, 0)
     }
 }
